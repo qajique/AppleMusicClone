5b65af2d41ecfe7fa9f2aa1f25c42f137cbe987c
Kazhymurat <komirzhan@gmail.com>
2022-05-03 14:18:59 +0600

    Initial Commit

---
+++ AppleMusicClone/AppleMusicCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicCloneApp.swift
+//  AppleMusicClone
+//
+//  Created by Kazhymurat on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
