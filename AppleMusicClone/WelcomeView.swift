c1a6e2cece39bea56b3168fb703233423a5a851a
Kazhymurat <komirzhan@gmail.com>
2022-05-22 17:02:00 +0600

    Add WelcomeView

---
+++ AppleMusicClone/WelcomeView.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeView.swift
+//  AppleMusicClone
+//
+//  Created by Kazhymurat on 22.05.2022.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            NavigationView {
+                VStack(alignment: .center) {
+                    Text("Ищете свою музыку?")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("Здесь появится купленная Вами в iTunes Store музыка")
+                        .font(.title2)
+                        .fontWeight(.regular)
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.center)
+                }
+                .navigationTitle("Медиатека")
+            }
+            PlayerView()
+            Divider()
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
